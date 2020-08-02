5ba20464f7a3d7d5db2882c1a2b0f5dc549b7829
Adamm <28355318+Adamm00@users.noreply.github.com>
2020-08-02 22:22:12 +1000

    Remove duplicate text

--- amcfwm.sh
+++ amcfwm.sh
@@ -1291,13 +1291,11 @@ case "$1" in
 		case "$2" in
 			srcloc)
 				if [ -z "$3" ]; then echo "[*] Source Location Not Specified - Exiting"; echo; exit 1; fi
-				if [ -z "$3" ]; then echo "[*] Source Location Doesn't Exist - Exiting"; echo; exit 1; fi
 				SRC_LOC="$3"
 				echo "[i] Source Location Set To $SRC_LOC"
 			;;
 			stageloc)
 				if [ -z "$3" ]; then echo "[*] Stage Location Not Specified - Exiting"; echo; exit 1; fi
-				if [ -z "$3" ]; then echo "[*] Stage Location Doesn't Exist - Exiting"; echo; exit 1; fi
 				STAGE_LOC="$3"
 				echo "[i] Stage Location Set To $STAGE_LOC"
 			;;
