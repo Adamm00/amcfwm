615c3dca1ccd557500ff05f0c24042e52857abed
Adamm <28355318+Adamm00@users.noreply.github.com>
2020-12-11 02:03:41 +1000

    Install automake-1.15

--- amcfwm.sh
+++ amcfwm.sh
@@ -1026,14 +1026,6 @@ case "$1" in
 
 				if [ "$LOCALFWVER" != "$REMOTEFWVER" ] || [ "$FORCEBUILD" = "y" ]; then
 					echo "*** $(date +%R) - Starting building $FWMODEL..."
-					if grep -qF "Ubuntu 20.04" "/etc/os-release"; then
-						echo "*** $(date +%R) - Patching wget"
-						cd "$HOME/$FWPATH/../src/router/wget" || exit 1
-						autoreconf -f -i >/dev/null 2>&1
-						echo "*** $(date +%R) - Patching tor"
-						cd "$HOME/$FWPATH/../src/router/tor" || exit 1
-						autoreconf -f -i >/dev/null 2>&1
-					fi
 					cd "$HOME/$FWPATH" || exit 1
 					if make "$FWMODEL" > "$HOME/amcfwm/$FWMODEL-output.txt" 2>&1; then
 						cd image || exit 1
@@ -1222,7 +1214,7 @@ case "$1" in
 			sudo dpkg --add-architecture i386
 			sudo apt-get update
 			sudo apt-get -y install "linux-headers-$(uname -r)"
-			sudo apt-get -y install lib32ncurses5-dev dos2unix libtool-bin cmake libproxy-dev uuid-dev liblzo2-dev autoconf automake bash bison bzip2 diffutils file flex m4 g++ gawk groff-base libncurses5-dev libtool libslang2 make patch perl pkg-config shtool subversion tar texinfo zlib1g zlib1g-dev git gettext libexpat1-dev libssl-dev cvs gperf unzip python libxml-parser-perl gcc-multilib gconf-editor libxml2-dev g++-multilib gitk libncurses5 mtd-utils libvorbis-dev autopoint autogen sed build-essential intltool libelf1 libglib2.0-dev xutils-dev lib32z1-dev lib32stdc++6 xsltproc gtk-doc-tools libelf-dev:i386 libelf1:i386 libltdl-dev openssh-server curl nano lzip patchelf
+			sudo apt-get -y install lib32ncurses5-dev dos2unix libtool-bin cmake libproxy-dev uuid-dev liblzo2-dev autoconf automake bash bison bzip2 diffutils file flex m4 g++ gawk groff-base libncurses5-dev libtool libslang2 make patch perl pkg-config shtool subversion tar texinfo zlib1g zlib1g-dev git gettext libexpat1-dev libssl-dev cvs gperf unzip python libxml-parser-perl gcc-multilib gconf-editor libxml2-dev g++-multilib gitk libncurses5 mtd-utils libvorbis-dev autopoint autogen sed build-essential intltool libelf1 libglib2.0-dev xutils-dev lib32z1-dev lib32stdc++6 xsltproc gtk-doc-tools libelf-dev:i386 libelf1:i386 libltdl-dev openssh-server curl nano lzip patchelf automake-1.15
 			sudo apt -y autoremove
 			if [ ! -f "$HOME/amcfwm/amcfwm.sh" ]; then curl -fsL --retry 3 "https://raw.githubusercontent.com/Adamm00/amcfwm/master/amcfwm.sh" -o "$HOME/amcfwm/amcfwm.sh"; fi
 			sudo ln -sf "$HOME/amcfwm/amcfwm.sh" /bin/amcfwm
